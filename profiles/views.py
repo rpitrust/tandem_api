a982a4788bbacb1a94da6db4337b006975a1e728
Krishna Aradhi <krishna@aradhi.me>
2014-08-24 19:26:33 -0400

    API endpoint for user registration

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,11 @@
+import json
+from django.http import HttpResponse
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -9,3 +14,24 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['POST'])
+def register(request):
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        print(serialized.init_data['first_name'])
+        print(serialized.init_data['last_name'])
+        user = User.objects.create_user(
+                   serialized.init_data['email'],
+                   serialized.init_data['username'],
+                   serialized.init_data['password'])
+        user.first_name = serialized.init_data['first_name']
+        user.last_name = serialized.init_data['last_name']
+        user.save()
+        data = {'token': Token.objects.get(user=user).key}
+        return HttpResponse(json.dumps(data), content_type='application/json',
+                            status=status.HTTP_201_CREATED)
+    else:
+        return HttpResponse(serialized._errors, content_type='application/json',
+                            status=status.HTTP_400_BAD_REQUEST)
--- tandem/urls.py
+++ tandem/urls.py
@@ -11,8 +11,11 @@ router.register(r'simresults', SimresultViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browseable API.
 urlpatterns = patterns('',
+    url(r'^users/register/', 'profiles.views.register', name='register'),
     url(r'^', include(router.urls)),
     url(r'^noise_vs_x/', 'simulations.views.noise_vs_x', name='noise_vs_x'),
+    url(r'^comm_per_sa/', 'simulations.views.comm_per_sa', name='comm_per_sa'),
+    # url(r'^sa_gain/', 'simulations.views.sa_gain', name='sa_gain'),
     url(r'^auth/', include('rest_framework.urls',
                        namespace='rest_framework')),
     url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
